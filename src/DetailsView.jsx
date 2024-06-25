7f74c4379b642913ddcdb04ea4521aba0f6c549b
Maximilian Piper <maximilianpiper@gmx.de>
2024-06-25 14:21:45 +0200

    change

--- src/DetailsView.jsx
+++ src/DetailsView.jsx
@@ -6,11 +6,9 @@ import PokemonDetails from "./PokemonDetails"
 function DetailsView() {
     const { name } = useParams()
     const [pokemDetails, setPokeDetails] = useState({});
-    const [pokeStats, setPokeStats] = useState([])
 
     useEffect(() => {
         fetchDetails(name).then((pokemonDetails) => setPokeDetails(pokemonDetails))
-        fetchDetails(name).then((pokemonDetails) => setPokeStats(pokemonDetails.stats))
     }, [])
 
     return <>
@@ -18,7 +16,7 @@ function DetailsView() {
         <h1>{name}</h1>
         <h2>height: {pokemDetails.height}</h2>
         <h2>weight: {pokemDetails.weight}</h2>
-        <PokemonDetails stat={pokeStats} />
+        <PokemonDetails stat={pokemDetails.stats} />
     </>
 
     
--- src/PokemonDetails.jsx
+++ src/PokemonDetails.jsx
@@ -2,7 +2,7 @@ function PokemonDetails(props) {
     const pokeDetails = props.stat;
 
     return <ul>
-        {pokeDetails.map((poke) => {
+        {pokeDetails?.map((poke) => {
         return (
           <div className='pokemon-id' key={poke.stat.name}>
             <h2 className="pokemon-stats">{poke.stat.name}: {poke.base_stat}</h2>
