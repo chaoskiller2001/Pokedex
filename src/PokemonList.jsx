00ae8724ae99b2a23ac102dbb6eeaef892c68332
Maximilian Piper <maximilianpiper@gmx.de>
2024-07-02 12:38:16 +0200

    favorites

--- src/PokemonList.jsx
+++ src/PokemonList.jsx
@@ -1,13 +1,19 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function PokemonList(props) {
     const poke = props.pokemon;
-
+    const [favorites, setFavorites] = useState([])
+    
+    function storeFavorites(props) {
+      let name = props
+      console.log(name)
+    }
     return <ul>
         {poke.map((pok) => {
         return (
           <div className='pokemon-id' key={pok.name}>
-            <Link className='pokemon-url' to={`details/${pok.name}`}>{pok.name}</Link>
+            <Link className='pokemon-url' to={`details/${pok.name}`}>{pok.name}</Link> <button onClick={storeFavorites(pok.name)}>add to favorites</button>
           </div>
         );
       })}
