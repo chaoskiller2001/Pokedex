a76df2c2c63c3d9c8ebfc84e1c16469e1a6cfe67
chaoskiller2001 <151149387+chaoskiller2001@users.noreply.github.com>
2025-06-23 10:32:17 +0000

    Image added for pokemon

--- src/DetailsView.jsx
+++ src/DetailsView.jsx
@@ -1,25 +1,26 @@
-import { useEffect, useState } from "react"
-import { Link, useParams } from "react-router-dom"
-import { fetchDetails } from "./pokemonService"
-import PokemonDetails from "./PokemonDetails"
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { fetchDetails } from "./pokemonService";
+import PokemonDetails from "./PokemonDetails";
 
 function DetailsView() {
-    const { name } = useParams()
-    const [pokemDetails, setPokeDetails] = useState({});
+  const { name } = useParams();
+  const [pokemDetails, setPokeDetails] = useState({});
 
-    useEffect(() => {
-        fetchDetails(name).then((pokemonDetails) => setPokeDetails(pokemonDetails))
-    }, [])
+  useEffect(() => {
+    fetchDetails(name).then((pokemonDetails) => setPokeDetails(pokemonDetails));
+  }, []);
 
-    return <>
-        <Link to="/">Back to list</Link>
-        <h1>{name}</h1>
-        <h2>height: {pokemDetails.height}</h2>
-        <h2>weight: {pokemDetails.weight}</h2>
-        <PokemonDetails stat={pokemDetails.stats} />
+  return (
+    <>
+      <Link to="/">Back to list</Link>
+      <h1>{name}</h1>
+      <img src={`https://img.pokemondb.net/artwork/${name}.jpg`}></img>
+      <h2>height: {pokemDetails.height}</h2>
+      <h2>weight: {pokemDetails.weight}</h2>
+      <PokemonDetails stat={pokemDetails.stats} />
     </>
-
-    
+  );
 }
 
-export default DetailsView
+export default DetailsView;
